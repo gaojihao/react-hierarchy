e9268f7a47c3dcddd21a67434121b05cd36a24eb
lizhi <zhi.li@wz-inc.com>
2020-04-23 18:53:39 +0800

    update style

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom'
-import { Layout, Row, Col, BackTop, Icon, Avatar} from 'antd';
-import './App.css'
+import { Layout, Row, Col, BackTop, Avatar} from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from '@ant-design/icons';
+import './App.css';
 import ZLMenu from './component/menu/menu';
 import menuConfig from './config/menu';
 import ZLBreadcrumb from './component/breadcrumb/Breadcrumb';
@@ -22,7 +26,7 @@ function App() {
             collapsed={collapsed}>
             <Row className="logo_container">
               <a href="/">
-                <span className="logo_title">Logger</span>
+                <span className="logo_title">管理系统</span>
               </a>
             </Row>
             <Row>
@@ -33,11 +37,9 @@ function App() {
             <Header style={{ backgroundColor: '#fff' }}>
               <Row align='middle' type='flex'>
                 <Col span={1}>
-                  <Icon
-                    className="trigger"
-                    type={collapsed ? 'menu-unfold' : 'menu-fold'}
-                    onClick={() => setCollapsed(!collapsed)}
-                  />
+                  {
+                    collapsed?<MenuUnfoldOutlined className="trigger" onClick={() => setCollapsed(!collapsed)}/>:<MenuFoldOutlined className="trigger" onClick={() => setCollapsed(!collapsed)}/>
+                  }
                 </Col>
                 <Col span={22}>
                   <ZLBreadcrumb />
