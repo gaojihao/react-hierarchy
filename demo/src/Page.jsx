c16c0ca92724390a873c533741f3be42b5c58a02
lizhi <zhi.li@wz-inc.com>
2020-04-23 16:08:37 +0800

    add router

---
+++ demo/src/Page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import App from './App';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+
+export default () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" render={() => <Redirect to="/app" push />}/>
+            <Route path="/app" component={App} />
+            <Route path="/404" component={NotFound} />
+            <Route path="/login" component={Login} />
+            <Route component={NotFound} />
+        </Switch>
+    </Router>
+);
