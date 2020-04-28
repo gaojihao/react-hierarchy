6159beb00482f2d8753ee02945e296874a0c7104
lizhi <zhi.li@wz-inc.com>
2020-04-28 20:15:35 +0800

    update datasource

--- demo/src/pages/Category/index.tsx
+++ demo/src/pages/Category/index.tsx
@@ -3,34 +3,25 @@ import {Table, Form, Input, Button} from 'antd';
 
 const dataSource = [
     {
-      key: '1',
+      categoryId: 1,
       name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号',
     },
     {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号',
+      categoryId: 2,
+      name: '胡彦斌',
     },
-  ];
+];
   
 const columns = [
     {
-      title: '姓名',
+      title: 'ID',
+      dataIndex: 'categoryId',
+      key: 'categoryId',
+    },
+    {
+      title: '类别',
       dataIndex: 'name',
       key: 'name',
-    },
-    {
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: '住址',
-      dataIndex: 'address',
-      key: 'address',
     },
 ];
 
