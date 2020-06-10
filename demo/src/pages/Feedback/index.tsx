e822fabf326d1c14c79cf648c70a0cc75e664a32
lizhi <lizhi1026@126.com>
2020-06-10 22:41:10 +0800

    update

--- demo/src/pages/Feedback/index.tsx
+++ demo/src/pages/Feedback/index.tsx
@@ -3,56 +3,60 @@ import {Table, Tag} from 'antd';
 
 const dataSource = [
     {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号',
-    },
+        name:'name',
+        phone:'phone',
+        type:1,
+        content:'center',
+        createTime:'2020-03-08',
+        fixed:1,
+    },{
+        name:'name',
+        phone:'phone',
+        type:1,
+        content:'center',
+        createTime:'2020-03-08',
+        fixed:1,
+    },{
+        name:'name',
+        phone:'phone',
+        type:1,
+        content:'center',
+        createTime:'2020-03-08',
+        fixed:1,
+    }
 ];
   
 const columns = [{
     title: '昵称',
     dataIndex: 'name',
     width: 1,
-    align: 'center'
 }, {
     title: '电话',
     dataIndex: 'phone',
     width: 1,
-    align: 'center'
 }, {
     title: '反馈类型',
     dataIndex: 'type',
     width: 1,
-    render: (type: string | number | undefined, record: { typeDesc: React.ReactNode; }, index: any) => (
+    render: (type: number | undefined, index: any) => (
         <span>
             <Tag color="red" key={type}>
-                {record.typeDesc}
+                {type}
             </Tag>
         </span>
     ),
-    align: 'center'
 }, {
     title: '内容',
     dataIndex: 'content',
     width: 1,
-    align: 'center'
 }, {
     title: '反馈时间',
     dataIndex: 'createTime',
     width: 1,
-    align: 'center'
 }, {
     title: '是否解决',
     dataIndex: 'fixed',
     width: 1,
-    align: 'center'
 }]
   
 
