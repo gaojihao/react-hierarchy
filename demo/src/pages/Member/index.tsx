eb11fc6f8650bb7c4c5f3b09a79843f861d3c081
lizhi <zhi.li@wz-inc.com>
2020-06-11 20:01:58 +0800

    member list style

--- demo/src/pages/Member/index.tsx
+++ demo/src/pages/Member/index.tsx
@@ -4,20 +4,34 @@ import { memberList } from '../../api/member';
   
   const columns = [
     {
-      title: '姓名',
-      dataIndex: 'nick_name',
-      key: 'nick_name',
-    },
-    {
       title: '手机号',
       dataIndex: 'phone',
       key: 'phone',
-    },
-    {
+    },{
+      title: '姓名',
+      dataIndex: 'nick_name',
+      key: 'nick_name',
+    },{
       title: '性别',
       dataIndex: 'sex',
       key: 'sex',
-    },
+    },{
+      title: '等级',
+      dataIndex: 'level',
+      key: 'level',
+    },{
+      title: '角色',
+      dataIndex: 'role',
+      key: 'role',
+    },{
+      title: '是否是vip',
+      dataIndex: 'vip',
+      key: 'vip',
+    },{
+      title: '用户Id',
+      dataIndex: 'user_id',
+      key: 'user_id',
+    }
   ];
   
 
