eb7167de6426ea1ffad6e9ab6300b3f2b00af018
lizhi <zhi.li@wz-inc.com>
2020-06-11 20:59:57 +0800

    upload picture

--- demo/src/pages/Picture/index.tsx
+++ demo/src/pages/Picture/index.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
-import {Table} from 'antd';
+import {Table, Upload, message} from 'antd';
+import { UploadChangeParam } from 'antd/lib/upload';
+import { InboxOutlined } from '@ant-design/icons';
+import './picture.css';
+
+const { Dragger } = Upload;
+
+const props = {
+  name: 'file',
+  multiple: false,
+  action: '/api/picture/upload',
+  onChange(info: UploadChangeParam) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
+
+
 
 const dataSource = [
     {
@@ -14,9 +39,9 @@ const dataSource = [
       age: 42,
       address: '西湖区湖底公园1号',
     },
-  ];
+];
   
-  const columns = [
+const columns = [
     {
       title: '姓名',
       dataIndex: 'name',
@@ -32,11 +57,21 @@ const dataSource = [
       dataIndex: 'address',
       key: 'address',
     },
-  ];
+];
   
 
 export default () => {
-    return (
-        <Table dataSource={dataSource} columns={columns} />
-    );
+  return (
+    <React.Fragment>
+      <div className="ant-upload-drag-dragger">
+        <Dragger {...props}>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">点击或拖拽上传</p>
+        </Dragger>
+      </div>
+      <Table dataSource={dataSource} columns={columns} />
+    </React.Fragment>
+  );
 };
