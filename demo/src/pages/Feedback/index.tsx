1a65409347aad9d983d75a715135c6ad9b0b9a42
lizhi <lizhi1026@126.com>
2020-06-12 22:07:35 +0800

    add feedback

--- demo/src/pages/Feedback/index.tsx
+++ demo/src/pages/Feedback/index.tsx
@@ -1,66 +1,62 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import {Table, Tag} from 'antd';
+import {FeedbackList,FixFeedback} from '../../api/feedback';
 
-const dataSource = [
-    {
-        name:'name',
-        phone:'phone',
-        type:1,
-        content:'center',
-        createTime:'2020-03-08',
-        fixed:1,
-    },{
-        name:'name',
-        phone:'phone',
-        type:1,
-        content:'center',
-        createTime:'2020-03-08',
-        fixed:1,
-    },{
-        name:'name',
-        phone:'phone',
-        type:1,
-        content:'center',
-        createTime:'2020-03-08',
-        fixed:1,
-    }
-];
   
 const columns = [{
-    title: '昵称',
-    dataIndex: 'name',
+    title: '电话',
+    dataIndex: 'mobile',
     width: 1,
 }, {
-    title: '电话',
-    dataIndex: 'phone',
-    width: 1,
+    title: '意见反馈',
+    dataIndex: 'content',
+    width: 4,
 }, {
     title: '反馈类型',
     dataIndex: 'type',
     width: 1,
-    render: (type: number | undefined, index: any) => (
-        <span>
-            <Tag color="red" key={type}>
-                {type}
-            </Tag>
-        </span>
-    ),
-}, {
-    title: '内容',
-    dataIndex: 'content',
-    width: 1,
+    render: (type: number) => {
+        if(type === 0){
+            return (<Tag color="orange">{type}</Tag>);
+        }else if(type === 2){
+            return (<Tag color="geekblue">{type}</Tag>);
+        }else{
+            return (<Tag color="pink">{type}</Tag>);
+        }
+    },
 }, {
     title: '反馈时间',
-    dataIndex: 'createTime',
+    dataIndex: 'create_time',
     width: 1,
 }, {
     title: '是否解决',
     dataIndex: 'fixed',
     width: 1,
+    render: (fixed: boolean) => (
+        <span>
+            <Tag color="red">
+                {fixed?'已修复':'未修复'}
+            </Tag>
+        </span>
+    ),
 }]
   
 
 export default () => {
+    const [dataSource, setDataSource] = useState();
+
+  useEffect(() => {
+    getFeedbackList();
+  },[]);
+
+  const getFeedbackList = () => {
+    FeedbackList().then((res) => {
+      setDataSource(res);
+    },onrejected => {
+      console.log(`onrejected=========${JSON.stringify(onrejected)}`);
+    })
+  };
+
     return (
         <Table dataSource={dataSource} columns={columns} />
     );
