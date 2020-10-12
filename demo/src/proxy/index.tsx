5f7594cd226b147246c0f155f0be66a9e920c278
lizhi <zhi.li@wz-inc.com>
2020-10-12 20:10:23 +0800

    update

---
+++ demo/src/proxy/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProxyPage = () => {
+
+    return (
+        <div style={{margin:'10px',display:'flex', alignContent:'center', justifyContent:'center'}}>
+            
+        </div>
+    );
+};
+
+export default ProxyPage;
